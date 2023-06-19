8a58f6a10764d122e7937341d033d1f4c0b64606
Shinigami <119001740+Shinigamiiz@users.noreply.github.com>
2023-06-19 15:45:09 +0530

    Create locale_db.py

---
+++ FallenRobot/modules/mongo/locale_db.py
@@ -0,0 +1,16 @@
+from pyrogram.enums import ChatType
+from typing import Iterable
+from FallenRobot.utils.mongo import db
+
+localesdb = db.locale  # DB for localization
+
+group_types: Iterable[ChatType] = (ChatType.GROUP, ChatType.SUPERGROUP)
+
+
+async def set_db_lang(chat_id: int, chat_type: str, lang_code: str):
+    await localesdb.update_one({"chat_id": chat_id}, {"$set": {"lang": lang_code, "chat_type": chat_type.value}}, upsert=True)
+
+
+async def get_db_lang(chat_id: int) -> str:
+    ul = await localesdb.find_one({"chat_id": chat_id})
+    return ul["lang"] if ul else {}
