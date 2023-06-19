38a02efa929f21323ec5d84312878250cd7c1dd3
Shinigami <119001740+Shinigamiiz@users.noreply.github.com>
2023-06-20 02:17:52 +0530

    Update welcome.py

--- FallenRobot/modules/welcome.py
+++ FallenRobot/modules/welcome.py
@@ -79,7 +79,7 @@ async def member_has_joined(_, member: ChatMemberUpdated):
             pass
     
     mention = f"<a href='tg://user?id={user.id}'>{user.first_name}</a>"
-    joined_date = datetime.fromtimestamp(time.time()).strftime("%Y.%m.%d %H:%M:%S")
+    joined_date = datetime.fromtimestamp(time.time()).strftime("%d %B %Y %I:%M:%S %p")
     first_name = f"{user.first_name} {user.last_name}" if user.last_name else user.first_name
     id = user.id
     dc = user.dc_id or "Member tanpa PP"
@@ -94,7 +94,7 @@ async def member_has_joined(_, member: ChatMemberUpdated):
     temp.MELCOW[f"welcome-{member.chat.id}"] = await app.send_photo(
         member.chat.id,
         photo=welcomeimg,
-        caption=f"Hai {mention}, <b>Welcome to the Group {member.chat.title}!</b>\n\n<b>Name:</b> <code>{first_name}</code>\n<b>ID:</b> <code>{id}</code>\n<b>DC ID:</b> <code>{dc}</code>\n<b>Join Date:</b> <code>{joined_date}</code>",
+        caption=f"Hey <b>{mention}</b>, <b>Welcome to the Group {member.chat.title}!</b>\n\n<b>Name:</b> <code>{first_name}</code>\n<b>ID:</b> <code>{id}</code>\n<b>Join Date:</b> <code>{joined_date}</code>",
     )
     
     try:
