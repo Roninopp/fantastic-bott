33ba1695a214b98fd6ef2bc6ab2e480226e455c2
Shinigami <119001740+Shinigamiiz@users.noreply.github.com>
2023-06-20 17:26:16 +0530

    Update sangmata_db.py

--- FallenRobot/modules/mongo/sangmata_db.py
+++ FallenRobot/modules/mongo/sangmata_db.py
@@ -2,27 +2,19 @@ from FallenRobot.utils.mongo import db
 
 matadb = db.sangmata
 
-
 # Get Data User
 async def cek_userdata(user_id: int) -> bool:
     user = await matadb.find_one({"user_id": user_id})
     return bool(user)
 
 
-async def get_enabled_chat_ids():
-    chats_cursor = matadb.find({"chat_id_toggle": {"$exists": True}})
-    chats = await chats_cursor.to_list(length=None)
-    return [chat["chat_id_toggle"] for chat in chats]
-
-async def get_userdata(user_id: int, chat_id: int):
-    user = await matadb.find_one({"user_id": user_id, "chat_id": chat_id})
-    if user:
-        return user.get("username"), user.get("first_name"), user.get("last_name")
-    return None, None, None
+async def get_userdata(user_id: int) -> bool:
+    user = await matadb.find_one({"user_id": user_id})
+    return user["username"], user["first_name"], user["last_name"]
 
 
-async def add_userdata(user_id: int, chat_id: int, username, first_name, last_name):
-    await matadb.update_one({"user_id": user_id, "chat_id": chat_id}, {"$set": {"username": username, "first_name": first_name, "last_name": last_name}}, upsert=True)
+async def add_userdata(user_id: int, username, first_name, last_name):
+    await matadb.update_one({"user_id": user_id}, {"$set": {"username": username, "first_name": first_name, "last_name": last_name}}, upsert=True)
 
 
 # Enable Mata MissKaty in Selected Chat
