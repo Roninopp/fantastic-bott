6ed256b418f2051ad71f9bef7e37cdf1055fb3f6
Shinigami <119001740+Shinigamiiz@users.noreply.github.com>
2023-06-20 11:55:32 +0530

    Update imposter.py

--- FallenRobot/modules/imposter.py
+++ FallenRobot/modules/imposter.py
@@ -4,57 +4,65 @@ from FallenRobot import pbot as app
 from FallenRobot.Pyro.permissions import adminsOnly
 from FallenRobot.Pyro.message_utils import kirimPesan
 
+user_data = {}
 
-async def send_name_change_message(chat_id, user, username, first_name, last_name):
-    msg = ""
-    old_user = await app.get_chat_member(chat_id, user.id)
-    if username != user.username or first_name != user.first_name or last_name != user.last_name:
-        msg += "👀 <b>Imposter Detected</b>\n\n"
-    if username != user.username:
-        msg += f"❇️ {user.mention} [<code>{user.id}</code>] changed username from @{username} to @{user.username}.\n"
-        await add_userdata(user.id, user.username, user.first_name, user.last_name)
-    if first_name != user.first_name:
-        msg += f"❇️ {user.mention} [<code>{user.id}</code>] changed first Name from {first_name} to {user.first_name}.\n"
-        await add_userdata(user.id, user.username, user.first_name, user.last_name)
-    if last_name != user.last_name:
-        msg += f"❇️ {user.mention} [<code>{user.id}</code>] changed last name from {last_name} to {user.last_name}."
-        await add_userdata(user.id, user.username, user.first_name, user.last_name)
-    if msg != "":
-        chat = await app.get_chat(chat_id)
-        await kirimPesan(chat, msg, quote=True)
-
-
-# Check user that change first_name, last_name and username
+# Check user that changes first_name, last_name, and username
 @app.on_message(filters.group & ~filters.bot & ~filters.via_bot, group=3)
 async def cek_mataa(_, m):
     if not await is_sangmata_on(m.chat.id):
         return
-    if not await cek_userdata(m.from_user.id):
-        await add_userdata(m.from_user.id, m.from_user.username, m.from_user.first_name, m.from_user.last_name)
+
+    chat_id = m.chat.id
+    user_id = m.from_user.id
+
+    if chat_id not in user_data:
+        user_data[chat_id] = {}
+
+    if user_id not in user_data[chat_id]:
+        user_data[chat_id][user_id] = {
+            "username": m.from_user.username,
+            "first_name": m.from_user.first_name,
+            "last_name": m.from_user.last_name,
+        }
     else:
-        username, first_name, last_name = await get_userdata(m.from_user.id)
-        if (
-            username != m.from_user.username
-            or first_name != m.from_user.first_name
-            or last_name != m.from_user.last_name
-        ):
-            await send_name_change_message(
-                m.chat.id, m.from_user, username, first_name, last_name
-            )
+        username = user_data[chat_id][user_id]["username"]
+        first_name = user_data[chat_id][user_id]["first_name"]
+        last_name = user_data[chat_id][user_id]["last_name"]
+        msg = ""
+
+        if username != m.from_user.username or first_name != m.from_user.first_name or last_name != m.from_user.last_name:
+            msg += "👀 <b>Imposter Detected</b>\n\n"
+
+        if username != m.from_user.username:
+            msg += f"❇️ {m.from_user.mention} [<code>{m.from_user.id}</code>] changed username from @{username} to @{m.from_user.username}.\n"
+            user_data[chat_id][user_id]["username"] = m.from_user.username
+
+        if first_name != m.from_user.first_name:
+            msg += f"❇️ {m.from_user.mention} [<code>{m.from_user.id}</code>] changed first Name from {first_name} to {m.from_user.first_name}.\n"
+            user_data[chat_id][user_id]["first_name"] = m.from_user.first_name
+
+        if last_name != m.from_user.last_name:
+            msg += f"❇️ {m.from_user.mention} [<code>{m.from_user.id}</code>] changed last name from {last_name} to {m.from_user.last_name}."
+            user_data[chat_id][user_id]["last_name"] = m.from_user.last_name
+
+        if msg != "":
+            await kirimPesan(m, msg, quote=True)
 
 
 @app.on_message(filters.group & filters.command("detectimposter") & ~filters.bot & ~filters.via_bot)
 @adminsOnly("can_change_info")
+@ratelimiter
 async def set_mataa(_, m):
     if len(m.command) == 1:
         return await kirimPesan(m, f"Use <code>/{m.command[0]} on</code>, to enable Imposter Detection. If you want to disable, you can use off parameter.")
+
     if m.command[1] == "on":
         cekset = await is_sangmata_on(m.chat.id)
         if cekset:
             await kirimPesan(m, "Imposter Detection already enabled in your group.")
         else:
             await sangmata_on(m.chat.id)
-            await kirimPesan(m, "Imposter Detection enabled in your group. I will track name and username changes in this chat. If user change their name and username, I will send a message showing any related changes")
+            await kirimPesan(m, "Imposter Detection enabled in your group. I will track name and username changes in this chat. If a user changes their name and username, I will send a message showing any related changes")
     elif m.command[1] == "off":
         cekset = await is_sangmata_on(m.chat.id)
         if not cekset:
@@ -69,5 +77,5 @@ async def set_mataa(_, m):
 __mod_name__ = "𝙳ᴇᴛᴇᴄᴛ 𝙸ᴍᴘᴏsᴛᴇʀ"
 __help__ = """
 
-*• /detectimposter:* Use this command to track name and username changes in group. If user change their name and username, I will send a message showing any related changes.
+*• /detectimposter:* Use this command to track name and username changes in the group. If a user changes their name and username, the bot will send a message showing any related changes.
 """
