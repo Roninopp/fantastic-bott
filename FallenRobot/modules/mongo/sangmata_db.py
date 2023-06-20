8f5476d003940de3c41c94185f6185b61a9ebd83
Shinigami <119001740+Shinigamiiz@users.noreply.github.com>
2023-06-20 16:25:37 +0530

    Update sangmata_db.py

--- FallenRobot/modules/mongo/sangmata_db.py
+++ FallenRobot/modules/mongo/sangmata_db.py
@@ -9,12 +9,6 @@ async def cek_userdata(user_id: int) -> bool:
     return bool(user)
 
 
-async def get_enabled_chat_ids() -> list[int]:
-    chats_cursor = matadb.find({"chat_id_toggle": {"$exists": True}})
-    chats = await chats_cursor.to_list(length=None)
-    return [chat["chat_id_toggle"] for chat in chats]
-
-
 async def get_userdata(user_id: int) -> bool:
     user = await matadb.find_one({"user_id": user_id})
     return user["username"], user["first_name"], user["last_name"]
