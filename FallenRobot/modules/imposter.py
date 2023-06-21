b4c8011db9166f3f8f921b2c87565377b8b0368b
Shinigami <119001740+Shinigamiiz@users.noreply.github.com>
2023-06-22 02:19:16 +0530

    Update imposter.py

--- FallenRobot/modules/imposter.py
+++ FallenRobot/modules/imposter.py
@@ -90,28 +90,29 @@ async def search_history(_, m):
             history_msg += f"<code>👤 {user_id}</code>\n\n"
 
             name_history = await history_db.find({"user_id": user_id}).sort("_id", -1).to_list(None)
+            name_history.reverse()  # Reverse the name history list
 
             previous_entry = None
 
-            name_history.reverse()
-for i, history in enumerate(name_history, start=1):
-    change_first_name = escape(history["first_name"])
-    change_last_name = escape(history["last_name"]) if history["last_name"] is not None else ""
-    change_username = escape(history["username"])
+            for i, history in enumerate(name_history, start=1):
+                change_first_name = escape(history["first_name"])
+                change_last_name = escape(history["last_name"]) if history["last_name"] is not None else ""
+                change_username = escape(history["username"])
 
-    if (change_first_name, change_last_name, change_username) != previous_entry:
-        timestamp = history["_id"].generation_time.astimezone(pytz.timezone("Asia/Kolkata"))
-        formatted_timestamp = timestamp.strftime("[<code>%d/%m/%Y %I:%M:%S %p</code>]")
+                if (change_first_name, change_last_name, change_username) != previous_entry:
+                    timestamp = history["_id"].generation_time.astimezone(pytz.timezone("Asia/Kolkata"))
+                    formatted_timestamp = timestamp.strftime("[<code>%d/%m/%Y %I:%M:%S %p</code>]")
 
-        history_msg += f"<b>{i}.</b> {formatted_timestamp}\n"
-        history_msg += f"   <code>{change_first_name}</code> <code>{change_last_name}</code>\n"
-        history_msg += f"   @{change_username}\n\n"
+                    history_msg += f"<b>{i}.</b> {formatted_timestamp}\n"
+                    history_msg += f"   <code>{change_first_name}</code> <code>{change_last_name}</code>\n"
+                    history_msg += f"   @{change_username}\n\n"
 
-    previous_entry = (change_first_name, change_last_name, change_username)
+                previous_entry = (change_first_name, change_last_name, change_username)
 
             await kirimPesan(m, history_msg, quote=True)
         else:
             await kirimPesan(m, "User data not found.")
+
 
 @app.on_message(filters.group & filters.command("detectimposter") & ~filters.bot & ~filters.via_bot)
 @adminsOnly("can_change_info")
