316de3185cd1d40a343a466376df292a3cb30cd9
Shinigami <119001740+Shinigamiiz@users.noreply.github.com>
2023-06-24 14:50:31 +0530

    Update fsub_db.py

--- FallenRobot/modules/mongo/fsub_db.py
+++ FallenRobot/modules/mongo/fsub_db.py
@@ -1,22 +1,18 @@
 from FallenRobot.utils.mongo import db
 
+fsub = db.force_sub
 
-fsubdb = db.fsub
 
-async def fsub_off(chat_id):
-    chat = await fsubdb.find_one({"chat_id" : chat_id})
-    if chat:
-        return await fsubdb.delete_one({"chat_id" : chat_id})
-
-async def fsub_on(chat_id : int, channel: str):
-    return await fsubdb.update_one({"chat_id" : chat_id},{"$set" : {"channel" : channel}}, upsert=True)
-
-async def fsub_stat(chat_id : int) -> bool:
-    chat = bool(await fsubdb.find_one({"chat_id" : chat_id}))
-    return chat 
-
-async def get_channel(chat_id : int):
-    chat = await fsubdb.find_one({"chat_id" : chat_id})
-    if chat:
-        return chat["channel"]
+def fs_settings(chat_id: int):
+    _x = fsub.find_one({"chat_id": chat_id})
+    if _x:
+        return _x
     return None
+
+
+def add_channel(chat_id: int, channel):
+    fsub.update_one({"chat_id": chat_id}, {"$set": {"channel": channel}}, upsert=True)
+
+
+def disapprove(chat_id: int):
+    fsub.delete_one({"chat_id": chat_id})
