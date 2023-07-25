ff021fe662299e438de2fecb12749b1fc7080476
VIVEK <99870677+Roninopp@users.noreply.github.com>
2023-07-25 10:24:17 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -99,7 +99,7 @@ buttons = [
 ]
 
 HELP_STRINGS = f"""
-[❃](https://telegra.ph/file/c12aededffbc29dd4080a.jpg)
+[Hey](https://telegra.ph/file/c12aededffbc29dd4080a.jpg)
 
  /help: PM's you this message.
  • /help <module name>: PM's you info about that module.
