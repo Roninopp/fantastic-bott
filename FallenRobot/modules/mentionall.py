902c5b99a2b07e2fe4dc36a4209a93ae4f6a55fa
VIVEK <99870677+Roninopp@users.noreply.github.com>
2023-07-27 11:48:48 +0530

    Update mentionall.py

--- FallenRobot/modules/mentionall.py
+++ FallenRobot/modules/mentionall.py
@@ -7,7 +7,7 @@ from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
 from telethon.tl.functions.channels import GetParticipantRequest
 from telethon.errors import UserNotParticipantError
 
-from GabiBraunRobot import telethn as Client
+from FallenRobot import telethn as Client
 
 spam_chats = []
 
