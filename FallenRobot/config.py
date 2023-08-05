daa484dbee8900752b3e86300246ef1bd65a91d2
VIVEK <99870677+Roninopp@users.noreply.github.com>
2023-08-05 23:51:43 +0530

    Update config.py

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -42,7 +42,7 @@ class Config(object):
     
   # Optional fields
     BL_CHATS = []  # List of groups that you want blacklisted.
-    DRAGONS = []  # User id of sudo users
+    DRAGONS = [5274479443]  # User id of sudo users
     DEV_USERS = [6171176459]  # User id of dev users
     DEMONS = []  # User id of support users
     TIGERS = []  # User id of tiger users
