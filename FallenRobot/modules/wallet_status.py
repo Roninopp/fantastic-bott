9235ca57477de0e6d8baf8a80da66c024c4f266c
VIVEK <99870677+Roninopp@users.noreply.github.com>
2023-08-26 17:57:00 +0530

    Create wallet_status.py

---
+++ FallenRobot/modules/wallet_status.py
@@ -0,0 +1,19 @@
+from FallenRobot.bot_class import Gojo as app
+from FallenRobot.planetScale_sqlDB.helper_functions.read import read
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from FallenRobot.planetScale_sqlDB.helper_functions.read import read
+from FallenRobot.utils.custom_filters import command
+import json
+
+
+reader = read()
+
+
+@app.on_message(command("wallet"))
+async def my_inventory(c: Client, m: Message):
+    user_id = m.from_user.id
+    rubies = reader.ruby(user_id)
+
+    message = f"Rubies 💵: {rubies}\n"
+    await m.reply_text(message)
