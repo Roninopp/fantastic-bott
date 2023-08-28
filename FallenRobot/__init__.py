fb633a75263f42469671047a3a0fc2605662422e
Krrish Kataria <109854409+Krrish557@users.noreply.github.com>
2023-08-28 21:35:20 +0530

    Update __init__.py

--- FallenRobot/__init__.py
+++ FallenRobot/__init__.py
@@ -15,6 +15,7 @@ from telethon import TelegramClient
 from aiohttp import ClientSession
 from Python_ARQ import ARQ
 from pyrogram.errors.exceptions.bad_request_400 import PeerIdInvalid, ChannelInvalid
+from config import configClass
 
 StartTime = time.time()
 
