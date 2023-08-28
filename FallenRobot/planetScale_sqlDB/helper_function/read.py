9b72c803c24dc2ecff90f66fa56440a6eb762f17
VIVEK <99870677+Roninopp@users.noreply.github.com>
2023-08-28 21:28:55 +0530

    Update read.py

--- FallenRobot/planetScale_sqlDB/helper_function/read.py
+++ FallenRobot/planetScale_sqlDB/helper_function/read.py
@@ -22,3 +22,17 @@ class read:
                 return 0
             else:
                 return pd[0]
+	def xp(self, user_id):
+    	with self.PLAYER_LOCK:
+            cur = con.cursor()
+            cur.execute(f'''
+                            SELECT xp 
+                            FROM playerDB 
+                            WHERE user_id = {user_id}
+                            ''')
+            pd = cur.fetchone()
+            cur.close()
+            if pd == None:
+                return 0
+            else:
+                return pd[0]
