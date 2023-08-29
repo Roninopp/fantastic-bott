d002b48b4fb3a6c40fc6aea52096de6caecf27bb
Krrish Kataria <109854409+Krrish557@users.noreply.github.com>
2023-08-29 20:18:01 +0530

    Update update.py

--- FallenRobot/planetScale_sqlDB/helper_function/update.py
+++ FallenRobot/planetScale_sqlDB/helper_function/update.py
@@ -1,38 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import threading
 from FallenRobot import con
 from FallenRobot.planetScale_sqlDB.playerDB import check_user
 
 
 class update:
+
     def __init__(self):
         self.PLAYER_LOCK = threading.RLock()
 
-    
     def add_money(self, user_id, amount):
         with self.PLAYER_LOCK:
-        cur = con.cursor()
+            cur = con.cursor()
 
             try:
-              if check_user(user_id):
-                  cur.execute('''
+                if check_user(user_id):
+                    cur.execute('''
                     UPDATE playerDB
                     SET ruby = %s
                     WHERE user_id = %s
-                ''', (amount, user_id))
-                  print("added money")
-                  con.commit()
+                ''',
+                                (amount, user_id))
+                    print 'added money'
+                    con.commit()
             finally:
-              cur.close()
+                cur.close()
+
     def add_xp(self, user_id, amount):
         with self.PLAYER_LOCK:
             try:
                 if check_user(user_id):
-                  cur = con.cursor()
-                  cur.execute('''
+                    cur = con.cursor()
+                    cur.execute('''
                   UPDATE playerDB
                   SET xp = %s
                   WHERE user_id = %s
-              ''', (amount, user_id))
-                  con.commit()
+              ''',
+                                (amount, user_id))
+                    con.commit()
             finally:
                 cur.close()
