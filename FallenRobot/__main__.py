74b720f17c1e35dd6bd069372b0206a509385e7a
VIVEK <99870677+Roninopp@users.noreply.github.com>
2023-09-03 20:41:47 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -818,7 +818,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "I Am Online Again To Save Your Group From Shogun")
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "I am Online Again To Safe Your Groups😃")
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!"
