ea7a0e6e89646a49f363d8e6d933fe3b6cabf3fb
VIVEK <99870677+Roninopp@users.noreply.github.com>
2023-09-30 14:21:49 +0530

    Update host.py

--- FallenRobot/modules/host.py
+++ FallenRobot/modules/host.py
@@ -1,6 +1,6 @@
 from telethon import events
 from . import telethn
 
-@telethn.on(events.NewMessage(pattern=r"^/(alive|about|hosting)")
+@telethn.on(events.NewMessage(pattern=r"^/(alive|about|hosting)"))
 async def about(event):
     await event.reply("Bot is working fine!\nHosted by @SpiralTechDivision")
