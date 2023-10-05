29d8e352d3ef8b74087e1223cd0c45bb64c78443
VIVEK <99870677+Roninopp@users.noreply.github.com>
2023-10-05 20:55:29 +0530

    Update config.py

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -28,12 +28,12 @@ class Config(object):
 
     SUPPORT_CHAT = "samurai_botsupport"  # Your Telegram support group chat username where your users will go and bother you
 
-    TOKEN = "2100096282:AAFAsMfN_f0ISmmeIHJzHYnUJh-VRxWIk1M"  # Get bot token from @BotFather on Telegram
+    TOKEN = "2100096282:AAFYKUba5wJWPvu4zJfAWkMWy6xcI66wFQM"  # Get bot token from @BotFather on Telegram
 
     TIME_API_KEY = ""  # Get this value from https://timezonedb.com/api
     SPAMWATCH_SUPPORT_CHAT = "effr"
     SPAMWATCH_API = "wdwd"
-    OWNER_ID = 1793699293  # User id of your telegram account (Must be integer)
+    OWNER_ID = 5965096598  # User id of your telegram account (Must be integer)
     OWNER_USERNAME = "DUSHMANxRONIN"
     WEBHOOK = False
     URL = "api.telegram.org"
@@ -42,9 +42,9 @@ class Config(object):
     
   # Optional fields
     BL_CHATS = []  # List of groups that you want blacklisted.
-    DRAGONS = [5274479443,5916768852,5410140404,6358950879]  # User id of sudo users
+    DRAGONS = []  # User id of sudo users
     DEV_USERS = []  # User id of dev users
-    DEMONS = [1737646273,1669062342,1811267624,1189238402]  # User id of support users
+    DEMONS = []  # User id of support users
     TIGERS = []  # User id of tiger users
     WOLVES = []  # User id of whitelist users
 
