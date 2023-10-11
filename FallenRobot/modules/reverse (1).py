18aa26363e19539910c51122733b4f318df39ffc
VIVEK <99870677+Roninopp@users.noreply.github.com>
2023-10-11 19:31:32 +0530

    Update reverse (1).py

--- FallenRobot/modules/reverse (1).py
+++ FallenRobot/modules/reverse (1).py
@@ -41,7 +41,7 @@ class STRINGS:
 🔗 <b>Page Link:</b> <a href="{page_url}">Link</a>
 
 ⌛️ <b>Time Taken:</b> <code>{time_taken}</code> seconds.
-🧑‍💻 <b>Chat Group:</b> @Hiddenleafvillagee
+🧑‍💻 <b>Chat Group:</b> @hidddenleeafvillage
     """
     OPEN_PAGE: str = "↗️ Open Page"
 
