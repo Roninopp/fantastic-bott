51dbc1314b7cae50b8b1f88c68ac535f3e5aec13
VIVEK <99870677+Roninopp@users.noreply.github.com>
2024-06-07 10:29:18 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -72,7 +72,7 @@ I have lots of handy features such as:
 buttons = [
     [
         InlineKeyboardButton(
-            text="➕ ADD FANTASTIC TO YOUR GROUP ➕", url="https://t.me/GANYUXBot?startgroup=new"),
+            text="➕ ADD FANTASTIC TO YOUR GROUP ➕", url="https://t.me/Fantasticfighterbot?startgroup=new"),
     ],
     [                  
                        InlineKeyboardButton(
