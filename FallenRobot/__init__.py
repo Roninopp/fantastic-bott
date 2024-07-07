d2bbbbb406018829004b92f8187249f87775632b
VIVEK <99870677+Roninopp@users.noreply.github.com>
2024-07-07 10:17:06 +0530

    Update __init__.py

--- FallenRobot/__init__.py
+++ FallenRobot/__init__.py
@@ -14,7 +14,7 @@ from aiohttp import ClientSession
 from Python_ARQ import ARQ
 from pyrogram.errors.exceptions.bad_request_400 import PeerIdInvalid, ChannelInvalid
 from FallenRobot.config import configClass
-
+app = DAXX()
 StartTime = time.time()
 
 # enable logging
